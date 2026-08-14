21fcc46d2da622ce457e302cc1e72e6baf44562a
ValeryFrenkel <valeron997@gmail.com>
2026-08-14 17:11:10 +0300

    Добавил тесты на форму логина

---
+++ tests/LoginTests.py
@@ -0,0 +1,22 @@
+import time
+
+from core.BaseTest import browser
+from pages.BasePage import BasePage
+from pages.LoginPage import LoginPageHelper
+
+BASE_URL = 'https://sn.rv-school.ru/'
+EMPTY_LOGIN_ERROR = 'Введите телефон, email или логин и пароль.'
+
+def test_empty_login_and_password(browser):
+    BasePage(browser).get_url(BASE_URL)
+    LoginPage = LoginPageHelper(browser)
+    LoginPage.click_login()
+    assert LoginPage.get_error_text() == EMPTY_LOGIN_ERROR
+
+def test_empty_password(browser):
+    BasePage(browser).get_url(BASE_URL)
+    LoginPage = LoginPageHelper(browser)
+    LoginPage.enter_login()
+    LoginPage.click_login()
+    time.sleep(5)
+    assert LoginPage.get_error_text() == EMPTY_LOGIN_ERROR
